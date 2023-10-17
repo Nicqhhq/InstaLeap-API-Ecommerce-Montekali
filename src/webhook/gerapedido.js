9469c8769deac196438d8f235f289977156414cd
Nícolas Pimenta Da Silva <99826916+Nicqhhq@users.noreply.github.com>
2023-10-17 11:37:54 -0300

    Adicionada Geração de Arquivo VD
    Adicionada a geração de arquivo para ser importado no pdv

--- src/webhook/gerapedido.js
+++ src/webhook/gerapedido.js
@@ -28,16 +28,79 @@ class Gerapedido {
         for (const codigos in itensvalor) {
             if (itensvalor.hasOwnProperty.call(itensvalor, codigos)) {
                 const produtos = itensvalor[codigos];
-                const itemean = produtos['ean'];
-                const itemquantidade = produtos['quantidade'] * 1000
-                const itemvalor = parseInt(produtos['valor'].toString().replace('.', ''))
-                const itemvalorformt = itemvalor < 1000 ? itemvalor * 10 : itemvalor
-                listaitem.push(`IT${itemean}00${itemquantidade}000000${itemvalorformt}`)
+                var itemean = produtos['ean'];
+                var itemquantidade = produtos['quantidade']
+                var itemvalor = parseInt(produtos['valor'].toString().replace('.', ''))
+                var itemunidade = produtos['unidade']
+                var linhaItemQuantidade = ``;
+                var linhaItemValor = ``;
+                console.log(`Valor Quantidade Sem Formatar ${itemquantidade}`);
+                console.log(`Valor Preco Sem Formatar ${itemvalor}`);
+                //validacao quantidade
+                if (itemunidade == 'KG') {
+                    itemquantidade = itemquantidade * 1000
+                    if (itemquantidade < 1000) {
+                        // console.log('Menor que 1000')
+                        linhaItemQuantidade = `000${itemquantidade}00`
+                    }
+                    else if (itemquantidade < 10000) {
+                        // console.log('menor que 10000')
+                        linhaItemQuantidade = `00${itemquantidade}00`
+                    }
+                    else if (itemquantidade < 100000) {
+                        // console.log('menor que 100000')
+                        linhaItemQuantidade = `00${itemquantidade}0`
+                    }
+                    else if (itemquantidade < 1000000) {
+                        // console.log('menor que 1000000')
+                        linhaItemQuantidade = `00${itemquantidade}`
+                    }
+                    // console.log(`KG IT${itemean}${linhaItemQuantidade}${linhaItemValor}`)
+                }
+                else {
+                    itemquantidade * 100
+                    if (itemquantidade < 1000) {
+                        // console.log('Menor que 1000')
+                        linhaItemQuantidade = `00${itemquantidade}00000`
+                    }
+                    else {
+                        // console.log('Maior que 1000')
+                        linhaItemQuantidade = `00${itemquantidade}0000`
+                    }
+                    // console.log(`UN IT${itemean}${linhaItemQuantidade}${linhaItemValor}`)
+                }
+                // tratativa valor
+                if (itemvalor < 100) {
+                    console.log("Valor menor que 100")
+                    linhaItemValor = `000000${itemvalor}`
+                }
+                else if (itemvalor < 1000) {
+                    console.log("Valor menor que 1000")
+                    linhaItemValor = `00000${itemvalor}`
+                }
+                else if (itemvalor < 10000) {
+                    console.log("Valor menor que 10000")
+                    linhaItemValor = `0000${itemvalor}`
+                }
+                else if (itemvalor < 100000) {
+                    console.log("Valor menor que 100000")
+                    linhaItemValor = `000${itemvalor}`
+                }
+                else if (itemvalor < 1000000) {
+                    console.log("Valor menor que 1000000")
+                    linhaItemValor = `00${itemvalor}`
+                }
+                else if (itemvalor < 10000000) {
+                    console.log("Valor menor que 10000000")
+                    linhaItemValor = `0${itemvalor}`
+                }
+                listaitem.push(`IT${itemean}${linhaItemQuantidade}${linhaItemValor}`)
             }
         }
         return [listaitem, numeropedido];
     }
     gravapedido(numeropedido, itensvalor) {
+        fs.appendFileSync(`${arquivopedido}VD${numeropedido}.txt`, `CL000000\n`,)
         var listaitem = this.getListaitem(numeropedido, itensvalor);
         for (const itens in listaitem[0]) {
             if (listaitem[0].hasOwnProperty.call(listaitem[0], itens)) {
